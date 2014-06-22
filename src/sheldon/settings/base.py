1882c570b0f1bd90a4178ff2eec04a7ea5bcf2b4
Torsten Rehn <trehn@case47.com>
2014-06-22 12:02:22 +0200

    add generic project skeleton

---
+++ src/sheldon/settings/base.py
@@ -0,0 +1,78 @@
+from os.path import dirname, join, realpath
+
+PROJECT_ROOT = realpath(dirname(dirname(__file__)))
+
+FIXTURE_DIRS = (
+    join(PROJECT_ROOT, "fixtures"),
+)
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.formtools',
+    'django.contrib.humanize',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'south',
+]
+
+LANGUAGE_CODE = "en-us"
+
+LOCALE_PATHS = (PROJECT_ROOT + "/locale",)
+
+MEDIA_ROOT = realpath(dirname(dirname(dirname(PROJECT_ROOT)))) + "/uploads"
+MEDIA_URL = "/uploads/"
+
+MIDDLEWARE_CLASSES = (
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+)
+
+ROOT_URLCONF = "sheldon.urls"
+
+SECRET_KEY = "FIXME"
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
+
+SITE_ID = 1
+
+STATIC_ROOT = realpath(dirname(dirname(PROJECT_ROOT))) + "/static"
+
+# remember this is hardcoded in the error page templates (e.g. 500.html)
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = (
+    join(PROJECT_ROOT, "static"),
+)
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+TEMPLATE_DIRS = (
+    join(PROJECT_ROOT, "templates")
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.csrf',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request',
+    'django.core.context_processors.static',
+    'django.contrib.messages.context_processors.messages',
+)
+
+USE_I18N = True
+USE_L10N = True
+USE_THOUSAND_SEPARATOR = False
---
+++ src/sheldon/settings/local.py
@@ -0,0 +1,44 @@
+from .base import *
+
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+]
+
+BASE_URL = "http://127.0.0.1:8000"
+
+DEPLOYMENT = 'local'
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+        'sheldon': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        }
+    }
+}
+
+MEDIA_ROOT = realpath(dirname(dirname(PROJECT_ROOT))) + "/uploads"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': realpath(dirname(dirname(PROJECT_ROOT))) + '/devdb.sqlite'
+    }
+}
