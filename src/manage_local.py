cb4e64e3c60319723dc20c0d36ff671f27f9baf8
Torsten Rehn <trehn@case47.com>
2014-06-26 23:54:07 +0200

    fix manage.py wrapper for schema migrations

--- src/manage_local.py
+++ src/manage_local.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     environ.setdefault("DJANGO_SETTINGS_MODULE", "sheldon.settings.local")
     environ.setdefault("SHELDON_CONFIG_FILE", join(dirname(dirname(__file__)), "sheldon.cfg"))
 
-    if len(argv) > 1 and not argv[1] in ("migrate", "syncdb"):
+    if len(argv) > 1 and not argv[1] in ("migrate", "schemamigration", "syncdb"):
         from sheldon.apps.secrets.utils import get_secret
         from django.conf import settings
         settings.SHELDON_SECRET_KEY = get_secret()
