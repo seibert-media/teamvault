749db15ee1687de6364c6cc175281daed5fd7669
Torsten Rehn <trehn@case47.com>
2014-06-27 17:10:31 +0200

    add audit log function

---
+++ src/sheldon/apps/audit/logging.py
@@ -0,0 +1,26 @@
+from logging import getLogger
+
+from .models import LogEntry
+
+
+AUDIT_LOG = getLogger(__name__)
+
+
+def log(
+    msg,
+    level='info',
+    actor=None,
+    password=None,
+    password_revision=None,
+    team=None,
+    user=None,
+):
+    getattr(AUDIT_LOG, level)(msg)
+    entry = LogEntry()
+    entry.message = msg
+    entry.actor = actor
+    entry.password = password
+    entry.password_revision = password_revision
+    entry.team = team
+    entry.user = user
+    entry.save()
