26261248587c8652ebfae7e98313c2c04f7d46e6
Torsten Rehn <trehn@case47.com>
2014-07-12 21:23:01 +0200

    password detail view

--- src/sheldon/apps/secrets/urls.py
+++ src/sheldon/apps/secrets/urls.py
@@ -2,13 +2,18 @@ from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
-from .views import live_search
+from .views import live_search, PasswordDetail
 
 urlpatterns = patterns('',
     url(
         r'^$',
         login_required(TemplateView.as_view(template_name="secrets/dashboard.html")),
         name='dashboard',
+    ),
+    url(
+        r'^passwords/(?P<pk>\d+)$',
+        login_required(PasswordDetail.as_view()),
+        name='secrets.password-detail',
     ),
     url(
         r'^passwords/live-search$',
--- src/sheldon/apps/secrets/views.py
+++ src/sheldon/apps/secrets/views.py
@@ -1,10 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from json import dumps
 
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from .models import Password
+
+
+class PasswordDetail(DetailView):
+    context_object_name = 'password'
+    model = Password
+    template_name = "secrets/password.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(PasswordDetail, self).get_context_data(**kwargs)
+        password = self.get_object()
+        context['readable'] = password.is_readable_by_user(self.request.user)
+        context['secret_url'] = reverse(
+            'api.password-revision_secret',
+            kwargs={'pk': password.current_revision.pk},
+        )
+        if context['readable']:
+            context['placeholder'] = password.current_revision.length * "•"
+        else:
+            context['placeholder'] = "<access denied>"
+        return context
+
+    def get_object(self):
+        object = super(PasswordDetail, self).get_object()
+        if not object.is_visible_to_user(self.request.user):
+            raise PermissionDenied()
+        return object
 
 
 class PasswordList(ListView):
@@ -37,7 +68,7 @@ def live_search(request):
     for password, icons in sorted_passwords:
         search_result.append((
             password.name,
-            "http://example.com",
+            reverse('secrets.password-detail', kwargs={'pk': password.pk}),
             icons,
         ))
 
