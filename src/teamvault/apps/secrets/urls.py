99931f64c86697f568d19c9ed4a2ef98e2529a91
Torsten Rehn <trehn@case47.com>
2014-11-17 20:52:05 +0100

    better view import in URLconf

--- src/teamvault/apps/secrets/urls.py
+++ src/teamvault/apps/secrets/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
-from .views import live_search, SecretAdd, SecretEdit, secret_delete, SecretDetail, SecretList
+from . import views
 
 urlpatterns = patterns('',
     url(
@@ -12,32 +12,32 @@ urlpatterns = patterns('',
     ),
     url(
         r'^secrets/$',
-        login_required(SecretList.as_view()),
+        login_required(views.SecretList.as_view()),
         name='secrets.secret-list',
     ),
     url(
         r'^secrets/(?P<pk>\d+)$',
-        login_required(SecretDetail.as_view()),
+        login_required(views.SecretDetail.as_view()),
         name='secrets.secret-detail',
     ),
     url(
         r'^secrets/(?P<pk>\d+)/delete$',
-        secret_delete,
+        views.secret_delete,
         name='secrets.secret-delete',
     ),
     url(
         r'^secrets/(?P<pk>\d+)/edit$',
-        login_required(SecretEdit.as_view()),
+        login_required(views.SecretEdit.as_view()),
         name='secrets.secret-edit',
     ),
     url(
         r'^secrets/add/(?P<content_type>\w+)$',
-        login_required(SecretAdd.as_view()),
+        login_required(views.SecretAdd.as_view()),
         name='secrets.secret-add',
     ),
     url(
         r'^secrets/live-search$',
-        live_search,
+        views.live_search,
         name='secrets.live-search',
     ),
 )
