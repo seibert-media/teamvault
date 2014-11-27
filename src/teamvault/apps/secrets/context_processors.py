77dfdaa31f9733411b8d10e08a1266c530e2ac1c
Torsten Rehn <trehn@case47.com>
2014-11-27 20:04:02 +0100

    don't try to count access requests when not logged in

--- src/teamvault/apps/secrets/context_processors.py
+++ src/teamvault/apps/secrets/context_processors.py
@@ -1,6 +1,8 @@
 from .models import AccessRequest
 
 def access_request_count(request):
+    if request.user.is_anonymous():
+        return {}
     return {
         'access_request_count': AccessRequest.objects.filter(
             reviewers=request.user,
