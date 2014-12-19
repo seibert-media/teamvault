5c1414c0f13990ae574d2d03e06b854067b8a1bb
Torsten Rehn <trehn@case47.com>
2014-12-19 16:23:29 +0100

    fix LDAP auth
    closes #6

--- src/teamvault/apps/settings/config.py
+++ src/teamvault/apps/settings/config.py
@@ -67,23 +67,24 @@ def configure_ldap_auth(config, settings):
     settings.AUTH_LDAP_USER_SEARCH = LDAPSearch(
         config.get("auth_ldap", "user_base_dn"),
         SEARCH_SCOPE_WHOLE_SUBTREE,
-        config.get("auth_ldap", "user_search_filter", "(cn=%(user)s)"),
+        get_from_config(config, "auth_ldap", "user_search_filter", "(cn=%(user)s)"),
     )
     settings.AUTH_LDAP_GROUP_SEARCH = LDAPSearch(
         config.get("auth_ldap", "group_base_dn"),
         SEARCH_SCOPE_WHOLE_SUBTREE,
-        config.get("auth_ldap", "group_search_filter", "(objectClass=group)"),
+        get_from_config(config, "auth_ldap", "group_search_filter", "(objectClass=group)"),
     )
 
     settings.AUTH_LDAP_GROUP_TYPE = MemberDNGroupType('member')
-    settings.AUTH_LDAP_REQUIRE_GROUP = config.get("auth_ldap", "require_group", None)
+    settings.AUTH_LDAP_REQUIRE_GROUP = get_from_config(config, "auth_ldap", "require_group", None)
 
     settings.AUTH_LDAP_USER_ATTR_MAP = {
-        "email": config.get("auth_ldap", "attr_email", "mail"),
-        "first_name": config.get("auth_ldap", "attr_first_name", "givenName"),
-        "last_name": config.get("auth_ldap", "attr_last_name", "sn"),
+        "email": get_from_config(config, "auth_ldap", "attr_email", "mail"),
+        "first_name": get_from_config(config, "auth_ldap", "attr_first_name", "givenName"),
+        "last_name": get_from_config(config, "auth_ldap", "attr_last_name", "sn"),
     }
     settings.AUTH_LDAP_USER_FLAGS_BY_GROUP = {
+        "is_staff": config.get("auth_ldap", "admin_group"),
         "is_superuser": config.get("auth_ldap", "admin_group"),
     }
 
