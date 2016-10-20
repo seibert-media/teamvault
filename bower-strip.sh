c7d370c9536d3fc11392913292cb77183059070b
Torsten Rehn <trehn@case47.com>
2016-10-20 19:47:07 +0200

    update frontend deps once again

--- bower-strip.sh
+++ bower-strip.sh
@@ -4,99 +4,79 @@
 BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 BUNDLED_DIR="${BASE_DIR}/src/teamvault/static/bundled"
 
+rm -rvf ${BUNDLED_DIR}/*/*.gemspec
+rm -rvf ${BUNDLED_DIR}/*/*.html
+rm -rvf ${BUNDLED_DIR}/*/*.map
+rm -rvf ${BUNDLED_DIR}/*/*.yml
+rm -rvf ${BUNDLED_DIR}/*/.*.yml
+rm -rvf ${BUNDLED_DIR}/*/.editorconfig
+rm -rvf ${BUNDLED_DIR}/*/.eslintrc
+rm -rvf ${BUNDLED_DIR}/*/.gitignore
+rm -rvf ${BUNDLED_DIR}/*/.jshintignore
+rm -rvf ${BUNDLED_DIR}/*/.jshintrc
+rm -rvf ${BUNDLED_DIR}/*/.npmignore
+rm -rvf ${BUNDLED_DIR}/*/AUTHORS.*
+rm -rvf ${BUNDLED_DIR}/*/CHANGELOG.*
+rm -rvf ${BUNDLED_DIR}/*/CONTRIBUTING.*
+rm -rvf ${BUNDLED_DIR}/*/dist/*.map
+rm -rvf ${BUNDLED_DIR}/*/dist/test
+rm -rvf ${BUNDLED_DIR}/*/doc
+rm -rvf ${BUNDLED_DIR}/*/docs
+rm -rvf ${BUNDLED_DIR}/*/Gemfile
+rm -rvf ${BUNDLED_DIR}/*/Gemfile.lock
+rm -rvf ${BUNDLED_DIR}/*/Gruntfile.js
+rm -rvf ${BUNDLED_DIR}/*/gulpfile.coffee
+rm -rvf ${BUNDLED_DIR}/*/ISSUE_TEMPLATE.*
+rm -rvf ${BUNDLED_DIR}/*/less
+rm -rvf ${BUNDLED_DIR}/*/Makefile
+rm -rvf ${BUNDLED_DIR}/*/package.json
+rm -rvf ${BUNDLED_DIR}/*/PULL_REQUEST_TEMPLATE.*
+rm -rvf ${BUNDLED_DIR}/*/README.*
+rm -rvf ${BUNDLED_DIR}/*/scss
+rm -rvf ${BUNDLED_DIR}/*/src
+rm -rvf ${BUNDLED_DIR}/*/test
+rm -rvf ${BUNDLED_DIR}/*/tests
+
+
 # bigtext
 rm -rvf ${BUNDLED_DIR}/bigtext/demo
 rm -rvf ${BUNDLED_DIR}/bigtext/grunt
-rm -rvf ${BUNDLED_DIR}/bigtext/src
-rm -rvf ${BUNDLED_DIR}/bigtext/test
-rm -rvf ${BUNDLED_DIR}/bigtext/Gruntfile.js
-rm -rvf ${BUNDLED_DIR}/bigtext/package.json
-rm -rvf ${BUNDLED_DIR}/bigtext/README.md
 
 # bootstrap
-rm -rvf ${BUNDLED_DIR}/bootstrap/CHANGELOG.md
 rm -rvf ${BUNDLED_DIR}/bootstrap/fonts
-rm -rvf ${BUNDLED_DIR}/bootstrap/Gemfile
-rm -rvf ${BUNDLED_DIR}/bootstrap/Gemfile.lock
 rm -rvf ${BUNDLED_DIR}/bootstrap/grunt
-rm -rvf ${BUNDLED_DIR}/bootstrap/Gruntfile.js
-rm -rvf ${BUNDLED_DIR}/bootstrap/ISSUE_TEMPLATE.md
 rm -rvf ${BUNDLED_DIR}/bootstrap/js
 rm -rvf ${BUNDLED_DIR}/bootstrap/less
-rm -rvf ${BUNDLED_DIR}/bootstrap/package.json
-rm -rvf ${BUNDLED_DIR}/bootstrap/README.md
 
 # card
-rm -rvf ${BUNDLED_DIR}/card/src
-rm -rvf ${BUNDLED_DIR}/card/gulpfile.coffee
-rm -rvf ${BUNDLED_DIR}/card/package.json
-rm -rvf ${BUNDLED_DIR}/card/README.md
-
-# font-awesome
-rm -rvf ${BUNDLED_DIR}/font-awesome/less
-rm -rvf ${BUNDLED_DIR}/font-awesome/scss
-rm -rvf ${BUNDLED_DIR}/font-awesome/.gitignore
-rm -rvf ${BUNDLED_DIR}/font-awesome/.npmignore
+rm -rvf ${BUNDLED_DIR}/card/webpack.config.js
 
 # jquery
-rm -rvf ${BUNDLED_DIR}/jquery/AUTHORS.txt
-rm -rvf ${BUNDLED_DIR}/jquery/LICENSE.txt
-rm -rvf ${BUNDLED_DIR}/jquery/README.md
-rm -rvf ${BUNDLED_DIR}/jquery/src
 
 # password-generator
-rm -rvf ${BUNDLED_DIR}/password-generator/*.html
 rm -rvf ${BUNDLED_DIR}/password-generator/*.js
-rm -rvf ${BUNDLED_DIR}/password-generator/*.yml
-rm -rvf ${BUNDLED_DIR}/password-generator/.gitignore
 rm -rvf ${BUNDLED_DIR}/password-generator/bin
 rm -rvf ${BUNDLED_DIR}/password-generator/dist/password-generator.js
 rm -rvf ${BUNDLED_DIR}/password-generator/lib
-rm -rvf ${BUNDLED_DIR}/password-generator/Makefile
-rm -rvf ${BUNDLED_DIR}/password-generator/test
 
 # select2
-rm -rvf ${BUNDLED_DIR}/select2/.gitignore
-rm -rvf ${BUNDLED_DIR}/select2/component.json
-rm -rvf ${BUNDLED_DIR}/select2/*.md
-rm -rvf ${BUNDLED_DIR}/select2/package.json
-rm -rvf ${BUNDLED_DIR}/select2/release.sh
-rm -rvf ${BUNDLED_DIR}/select2/select2.js
-rm -rvf ${BUNDLED_DIR}/select2/select2_locale_*
+rm -rvf ${BUNDLED_DIR}/select2/docs
+
+# font-awesome
+rm -rvf ${BUNDLED_DIR}/font-awesome/HELP-US-OUT.txt
 
 # select2-bootstrap
 rm -rvf ${BUNDLED_DIR}/select2-bootstrap/_jekyll
 rm -rvf ${BUNDLED_DIR}/select2-bootstrap/compass
-rm -rvf ${BUNDLED_DIR}/select2-bootstrap/docs
 rm -rvf ${BUNDLED_DIR}/select2-bootstrap/lib
-rm -rvf ${BUNDLED_DIR}/select2-bootstrap/test
-rm -rvf ${BUNDLED_DIR}/select2-bootstrap/Gemfile
-rm -rvf ${BUNDLED_DIR}/select2-bootstrap/Gruntfile.js
-rm -rvf ${BUNDLED_DIR}/select2-bootstrap/Makefile
-rm -rvf ${BUNDLED_DIR}/select2-bootstrap/package.json
-rm -rvf ${BUNDLED_DIR}/select2-bootstrap/README.md
-rm -rvf ${BUNDLED_DIR}/select2-bootstrap/select2-bootstrap-css.gemspec
 
 # typeahead.js
-rm -rvf ${BUNDLED_DIR}/typeahead.js/doc
-rm -rvf ${BUNDLED_DIR}/typeahead.js/src
-rm -rvf ${BUNDLED_DIR}/typeahead.js/test
-rm -rvf ${BUNDLED_DIR}/typeahead.js/.gitignore
-rm -rvf ${BUNDLED_DIR}/typeahead.js/.jshintrc
-rm -rvf ${BUNDLED_DIR}/typeahead.js/.travis.yml
 rm -rvf ${BUNDLED_DIR}/typeahead.js/composer.json
-rm -rvf ${BUNDLED_DIR}/typeahead.js/*.md
-rm -rvf ${BUNDLED_DIR}/typeahead.js/Gruntfile.js
 rm -rvf ${BUNDLED_DIR}/typeahead.js/karma.conf.js
-rm -rvf ${BUNDLED_DIR}/typeahead.js/package.json
 rm -rvf ${BUNDLED_DIR}/typeahead.js/typeahead.js.jquery.json
 
 # underscore
-rm -rvf ${BUNDLED_DIR}/underscore/.eslintrc
-rm -rvf ${BUNDLED_DIR}/underscore/.gitignore
 rm -rvf ${BUNDLED_DIR}/underscore/component.json
-rm -rvf ${BUNDLED_DIR}/underscore/package.json
-rm -rvf ${BUNDLED_DIR}/underscore/README.md
 rm -rvf ${BUNDLED_DIR}/underscore/underscore.js
 
 # zeroclipboard
