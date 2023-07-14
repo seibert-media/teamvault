36cce43b2e481cf647bafc81b4e8274262c5921a
Marcel Reinhardt <mreinhardt@seibert-media.net>
2023-07-14 14:19:11 +0200

    save_*avatar: create UserSettings object if it does not exist yet

--- teamvault/apps/accounts/utils.py
+++ teamvault/apps/accounts/utils.py
@@ -3,17 +3,21 @@ from hashlib import md5
 
 import requests
 
+from teamvault.apps.accounts.models import UserSettings as UserSettingsModel
+
 
 def save_gravatar(user, *_args, **_kwargs):
     email_hash = md5(user.email.strip().lower().encode("utf-8")).hexdigest()
     resp = requests.get(f'https://gravatar.com/avatar/{email_hash}?s=200&r=g&d=mp')
     if resp.ok:
-        user.profile.avatar = b64encode(resp.content)
-        user.profile.save()
+        user_settings = UserSettingsModel.objects.get_or_create(user=user)[0]
+        user_settings.avatar = b64encode(resp.content)
+        user_settings.save()
 
 
 def save_google_avatar(response, user, *_args, **_kwargs):
     resp = requests.get(response['picture'])
     if resp.ok:
-        user.profile.avatar = b64encode(resp.content)
-        user.profile.save()
+        user_settings = UserSettingsModel.objects.get_or_create(user=user)[0]
+        user_settings.avatar = b64encode(resp.content)
+        user_settings.save()
