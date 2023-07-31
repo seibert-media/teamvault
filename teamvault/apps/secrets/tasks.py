f7c2cefd928581649b7900e14b32802bd8fadd75
dmarkov <markovs.code@gmail.com>
2023-07-31 12:25:51 +0200

    Revoke unused access as huey task once a day

--- teamvault/apps/secrets/tasks.py
+++ teamvault/apps/secrets/tasks.py
@@ -3,6 +3,8 @@ import logging
 from huey import crontab
 from huey.contrib.djhuey import db_periodic_task
 
+from teamvault.apps.secrets.revoke_access import revoke_unused_access
+
 huey_log = logging.getLogger('huey')
 
 
@@ -10,3 +12,8 @@ huey_log = logging.getLogger('huey')
 def add(a=1, b=5):
     huey_log.info(f'Whoah {a} + {b} is {a + b}')
     return a + b
+
+
+@db_periodic_task(crontab(minute=0, hour=0))
+def periodic_revoke_unused_access():
+    revoke_unused_access()
