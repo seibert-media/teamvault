ec3e4c7f5150b84e976eaf9092257cb203ca7094
Marcel Reinhardt <mreinhardt@seibert-media.net>
2024-01-05 10:08:12 +0100

    Support more JS builders

--- build.sh
+++ build.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-printf ">> Creating webpack bundle...\n\n"
-yarn run build
+if command -v bun; then
+    BUILDER="bun" 
+elif command -v yarn; then
+    BUILDER="yarn"
+else
+    BUILDER="npm"
+fi
+
+printf ">> Creating webpack bundle via $BUILDER...\n\n"
+$BUILDER run build
 [ $? -eq 0 ] && printf "\n>> Bundle created.\n"
 
 printf ">> Generating python package...\n\n"
 python3 -m build
-printf "\n>> Generated python package in dist/."
+[ $? -eq 0 ] && printf "\n>> Generated python package in dist/."
