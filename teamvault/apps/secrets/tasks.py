32b4b103e2691c780f7419d1412b494af2fce0c7
Peter Hofmann <peter.hofmann@seibert.group>
2024-02-06 08:40:31 +0100

    Revoke unused shares

--- teamvault/apps/secrets/tasks.py
+++ teamvault/apps/secrets/tasks.py
@@ -1,7 +1,14 @@
+from datetime import timedelta
+
 import logging
 
+from ..audit.auditlog import log
+from ..audit.models import LogEntry, AuditLogCategoryChoices
 from ..secrets.models import SharedSecretData
 
+from django.conf import settings
+from django.contrib.auth.models import Group
+from django.utils.timezone import now
 from django.utils.translation import gettext_lazy as _
 from huey import crontab
 from huey.contrib.djhuey import periodic_task
@@ -23,3 +30,49 @@ def prune_expired_shares():
             ),
         )
         share.delete()
+
+
+@periodic_task(crontab(**settings.HUEY_TASK_FREQUENCY))
+def revoke_unused_shares():
+    if not hasattr(settings, 'REVOKE_UNUSED_SHARES_AFTER_DAYS'):
+        return
+
+    grace_period = now() - timedelta(days=settings.REVOKE_UNUSED_SHARES_AFTER_DAYS)
+
+    for share in SharedSecretData.objects.filter(
+        granted_on__lt=grace_period,
+    ):
+        users_to_check = []
+
+        if share.user:
+            users_to_check.append(share.user)
+        else:
+            users_to_check.extend(share.group.user_set.all())
+
+        do_revoke = True
+        for user in users_to_check:
+            accessed = LogEntry.objects.filter(
+                actor=user,
+                secret=share.secret,
+                time__gte=grace_period,
+            )
+            if accessed:
+                do_revoke = False
+                # Skip further unnecessary checks.
+                break
+
+        if do_revoke:
+            log(
+                _(
+                    "Share for {share_type} '{who}' automatically revoked, "
+                    "not used since {grace_period}"
+                ).format(
+                    grace_period=grace_period,
+                    share_type=_("user") if share.user else _("group"),
+                    who=share.user or share.group,
+                ),
+                category=AuditLogCategoryChoices.MISCELLANEOUS,
+                level='info',
+                secret=share.secret,
+            )
+            share.delete()
