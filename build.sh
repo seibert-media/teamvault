378801e84392f9f195eea36adf00599bc4a88461
Marcel Reinhardt <mreinhardt@seibert-media.net>
2024-03-21 19:07:18 +0100

    Clean up old artefacts on new builds

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
-if command -v bun; then
+if $(command -v bun); then
     BUILDER="bun" 
-elif command -v yarn; then
+elif $(command -v yarn); then
     BUILDER="yarn"
 else
     BUILDER="npm"
 fi
+
+PROJECT_DIR="$(dirname "$0")"
+PKG_DIR="$PROJECT_DIR/dist"
+
+cd "$PROJECT_DIR"
+
+printf ">> Cleaning up old build packages...\n\n"
+[ -e "$PKG_DIR" ] && printf "Deleting existing dist in $PKG_DIR...\n" && rm -r "$PKG_DIR"
 
 printf ">> Creating webpack bundle via $BUILDER...\n\n"
 $BUILDER run build
