a18d2e1713734c1fe535da6a4cb157513e01b62d
Gianluca Lefebvre <gianluca.lefebvre@seibert.group>
2024-07-29 11:22:02 +0200

    Fixed positioning of OTP timer in big mode and corrected loading of password and otp key

--- teamvault/apps/secrets/api/views.py
+++ teamvault/apps/secrets/api/views.py
@@ -152,7 +152,10 @@ def data_get(request, hashid):
     secret_revision = get_object_or_404(SecretRevision, hashid=hashid)
     secret_revision.secret.check_read_access(request.user)
     data = secret_revision.secret.get_data(request.user)
-    key = loads(secret_revision.secret.get_data(request.user, get_otp_key=True))
+    if secret_revision.encrypted_otp_key_data:
+        key = loads(secret_revision.secret.get_data(request.user, get_otp_key=True))
+    else:
+        key = None
     if secret_revision.secret.content_type == Secret.CONTENT_PASSWORD:
         return Response({'password': data, 'otp-key-data': key})
     elif secret_revision.secret.content_type == Secret.CONTENT_FILE:
