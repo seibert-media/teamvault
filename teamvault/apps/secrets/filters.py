ec6a317a5df47c9c2764c18f38b138703493b1b0
Dimitar Markov <markovs.code@gmail.com>
2025-02-20 09:51:00 +0100

    Set up active filter labels for Secrets

--- teamvault/apps/secrets/filters.py
+++ teamvault/apps/secrets/filters.py
@@ -3,13 +3,25 @@ import enum
 import django_filters
 from django.contrib.auth import get_user_model
 from django import forms
-from django.db.models import IntegerChoices
+from django.db.models import IntegerChoices, TextChoices
+from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 
 from teamvault.apps.secrets.models import Secret
 
 User = get_user_model()
+
+
+def add_tooltip(label, tooltip_message):
+    return format_html(
+        '{} '
+        '<i class="fa fa-exclamation-circle fa-fw opacity-75"'
+        ' data-bs-toggle="tooltip" data-bs-placement="top" title="{}">'
+        '</i>',
+        label,
+        tooltip_message
+    )
 
 
 class Icons(enum.Enum):
@@ -24,7 +36,7 @@ class Icons(enum.Enum):
         return f'<i class="fa fa-fw {self.value}"></i> '
 
 
-class ContentChoices(IntegerChoices):
+class ContenTypeChoices(IntegerChoices):
     # TODO: Merge CONTENT_* vars with these ones.
     #  Preferably migrate occurances of Secret.CONTENT_CHOICES to this class
     PASSWORD = Secret.CONTENT_PASSWORD, mark_safe(Icons.KEY.html + _('Password'))
@@ -37,12 +49,15 @@ class StatusChoices(IntegerChoices):
     #  Preferably migrate occurances of Secret.STATUS_CHOICES to this class
     OK = Secret.STATUS_OK, mark_safe(Icons.KEY.html + _('Regular'))
     NEEDS_CHANGING = Secret.STATUS_NEEDS_CHANGING, mark_safe(Icons.REFRESH_DANGER.html + _('Needs Changing'))
-    DELETED = Secret.STATUS_DELETED, mark_safe(Icons.DELETED_DANGER.html + _('Deleted'))
+    DELETED = Secret.STATUS_DELETED, mark_safe(Icons.DELETED_DANGER.html) + f'{add_tooltip( 
+        _('Deleted'),
+        _('Hide deleted secrets per default by changing your settings.')
+    )}'
 
 
 class SecretFilter(django_filters.FilterSet):
     content_type = django_filters.MultipleChoiceFilter(
-        choices=ContentChoices.choices,
+        choices=ContenTypeChoices,
         widget=forms.CheckboxSelectMultiple,
         label='Type'
     )
