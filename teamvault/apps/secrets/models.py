9f5533d53ce088b4309dae423828604679fa7010
Marcel Reinhardt <mreinhardt@seibert-media.net>
2025-07-02 00:34:19 +0200

    Don't force OTP keys on Secret.set_data()

--- teamvault/apps/secrets/models.py
+++ teamvault/apps/secrets/models.py
@@ -438,8 +438,10 @@ class Secret(HashIDModel):
                 plaintext_data["password"] = old_data["password"]
             if not set_otp and "otp_key" in old_data:
                 for key in ["otp_key", "digits", "algorithm"]:
-                    plaintext_data[key] = old_data[key]
-                    set_otp = True
+                    if key in old_data:
+                        plaintext_data[key] = old_data[key]
+                        set_otp = True
+
         if self.content_type == Secret.CONTENT_PASSWORD and "password" in plaintext_data.keys():
             plaintext_length = len(plaintext_data["password"])
         if set_otp:
