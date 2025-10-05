b45d8300ea39b0c9698ef4baabfb69f374fc4669
Dimitar Markov <markovs.code@gmail.com>
2025-10-05 19:06:42 +0300

    Adjust test_constraints to PR #181

--- teamvault/apps/secrets/tests/models/test_constraints.py
+++ teamvault/apps/secrets/tests/models/test_constraints.py
@@ -2,21 +2,23 @@ from django.db import IntegrityError
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
-from teamvault.apps.secrets.models import SharedSecretData
-from teamvault.apps.secrets.tests.helpers import make_secret
 
+from teamvault.apps.secrets.models import SharedSecretData
+from ..utils import make_user, new_secret
 User = get_user_model()
 
 class ShareConstraintsTests(TestCase):
     def setUp(self):
-        self.owner = User.objects.create_user("owner")
-        self.u = User.objects.create_user("other")
+        self.owner = make_user("owner")
+        self.u = make_user("other")
         self.g = Group.objects.create(name="g")
-        self.s = make_secret(self.owner, share_with_owner=False)
+        self.s = new_secret(self.owner, share_with_owner=False)
 
     def test_only_one_of_user_or_group(self):
         with self.assertRaises(IntegrityError):
-            SharedSecretData.objects.create(secret=self.s, user=self.u, group=self.g, granted_by=self.owner)
+            SharedSecretData.objects.create(
+                secret=self.s, user=self.u, group=self.g, granted_by=self.owner
+            )
 
     def test_unique_user_secret_pair(self):
         SharedSecretData.objects.create(secret=self.s, user=self.u, granted_by=self.owner)
