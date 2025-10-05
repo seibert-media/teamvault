0d4387a76bbd148b047bb7be1f0c050279c10984
Dimitar Markov <markovs.code@gmail.com>
2025-10-05 19:00:26 +0300

    Adjust encryption tests to PR #181

--- teamvault/apps/secrets/tests/models/test_encryption.py
+++ teamvault/apps/secrets/tests/models/test_encryption.py
@@ -1,31 +1,32 @@
 from django.test import TestCase, override_settings
-from django.contrib.auth import get_user_model
-
 from cryptography.fernet import Fernet, InvalidToken
 
-from teamvault.apps.secrets.models import Secret, SharedSecretData
-from teamvault.apps.secrets.exceptions import PermissionError
-from teamvault.apps.secrets.tests.helpers import STATIC_TEST_KEY
-
-User = get_user_model()
+from teamvault.apps.secrets.models import Secret
+from teamvault.apps.secrets.services.revision import RevisionService
+from ..utils import COMMON_OVERRIDES, make_user, new_secret
 
 
-@override_settings(TEAMVAULT_SECRET_KEY=STATIC_TEST_KEY)
+@override_settings(**COMMON_OVERRIDES)
 class SecretEncryptionTests(TestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(username="alice", password="x")
-        self.secret = Secret.objects.create(name="enc", created_by=self.user)
-        self.secret.set_data(self.user, {"password": "password"}, skip_access_check=True)
-        SharedSecretData.objects.create(secret=self.secret, user=self.user, granted_by=self.user)
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = make_user("alice")
+        cls.secret: Secret = new_secret(cls.user, name="enc")
+        RevisionService.save_payload(
+            secret=cls.secret,
+            actor=cls.user,
+            payload={"password": "password"},
+        )
 
     def test_round_trip_decrypt_returns_original(self):
         data = self.secret.get_data(self.user)
         self.assertEqual(data["password"], "password")
 
-    def test_ciphertext_not_plaintext(self):
+    def test_ciphertext_does_not_contain_plaintext(self):
+        plaintext = self.secret.get_data(self.user)["password"].encode("utf-8")
         blob = self.secret.current_revision.encrypted_data
         self.assertIsInstance(blob, (bytes, bytearray))
-        self.assertNotIn(b"password", blob)
+        self.assertNotIn(plaintext, blob)
 
     def test_wrong_key_raises_invalidtoken(self):
         other_key = Fernet.generate_key()
